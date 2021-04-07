f63baf81061e5b17127a21a9c88de43e5d79efde
Nathan Donolli <ndonolli@gmail.com>
2021-04-06 22:06:02 -0500

    init

---
+++ src/main/melissums/core.cljs
@@ -0,0 +1,13 @@
+(ns melissums.core
+  (:require [reagent.dom :as dom]))
+
+(defn hello []
+  [:h1 "hello"])
+
+(defn ^:dev/after-load start []
+  (dom/render
+   [hello]
+   (.getElementById js/document "app")))
+
+(defn ^:export main []
+  (start))
