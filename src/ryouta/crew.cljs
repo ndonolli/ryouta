0d805ffc230b08c4d63710e62bd2c93226bc4da2
Nathan Donolli <ndonolli@aligntoday.com>
2021-04-26 03:14:28 -0500

    add typewriter component

--- src/ryouta/crew.cljs
+++ src/ryouta/crew.cljs
@@ -1,13 +1,29 @@
 (ns ryouta.crew
   (:require [reagent.dom :as dom]
+            [reagent.core :as r]
             [ryouta.state :as state :refer [db]]
             [ryouta.director :as direct]))
+
+(defn typewriter [text delay] 
+  (let [typed (r/atom "")
+        prev-text (r/atom "")]
+    (letfn [(type* [chars]
+              (swap! typed str (first chars))
+              (if-let [next (seq (rest chars))]
+                (js/setTimeout #(type* next) delay)
+                  (js/console.log "done")))]
+      (fn [text delay]
+        (when-not (= text @prev-text)
+          (reset! prev-text text)
+          (reset! typed "")
+          (type* (seq text)))
+        [:span @typed]))))
 
 (defn dialogue []
   (when (:visible @state/dialogue)
     [:div.ry-dialogue
      [:div.ry-dialogue-title (:actor @state/dialogue)]
-     [:div.ry-dialogue-content (:line @state/dialogue)]]))
+     [:div.ry-dialogue-content [typewriter (:line @state/dialogue) 50]]]))
 
 (defn actors []
   [:div.ry-actors
